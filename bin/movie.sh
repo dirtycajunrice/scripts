f95fc57fcce4f1c18eaf770d70482a0a965c7651
Nicholas Totsch <nicholas.totsch@gmail.com>
2016-08-16 01:55:25 -0500

    Slowly updating

--- bin/movie.sh
+++ bin/movie.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 
-# Exported Variablees
-export d=/tmp/movielists
-export halp="You must have 1 or more arguments. Type -h or --help for help with arguments"
-export spacer="---------------------------------------------------"
-export arg2="$2"
-export quality='360p 480p 720p 1080p 2160p'
+# Variablees
+d=/tmp/movielists
+halp="You must have 1 or more arguments. Type -h or --help for help with arguments"
+spacer="---------------------------------------------------"
+arg2="$2"
+quality='360p 480p 720p 1080p 2160p'
 
 # Functions
-
 moviestats() {  # Sorts all movies into seperate quality lists and prints results to the terminal.
-echo $spacer
-echo "Creating $d..."
-if [ -d "$d" ]; then
-    echo "$d already existed"
-else
+if [ ! -d "$d" ]; then
     mkdir $d
     echo "Created $d"
 fi
 echo $spacer
-echo "Updating lists..."
 if [ ! -w $d/movies.list ]; then
     echo "Created movies.list"
     ls /movies /movies2 > $d/movies.list
@@ -27,7 +21,6 @@ if [ ! -w $d/movies.list ]; then
 else
     ls /movies /movies2  > $d/movies.list
     sed -i -e '/^$/d' -e '/^\//d' $d/movies.list
-    echo "Updated maser movies list"
     num=$(wc -l < "$d"/movies.list)
     echo "You have $num movies"
 fi
@@ -36,30 +29,20 @@ grep -vE '(2160|1080|720|480|360)p' $d/movies.list > $d/oddquality.list
 
 if ! grep -qvE '(2160|1080|720|480|360)p' $d/movies.list; then
     rm $d/oddquality.list
-    echo "You have no odd quality movies! Fuck Yeah!"
+    echo "You have no odd quality movies"
 else
-    echo "Updated the Odd Quality List"
     num=$(wc -l < $d/oddquality.list)
-    echo "You have $num Odd Quality movies left to upgrade"
+    echo "You have $num Odd Quality movies"
 fi
 for list2 in $quality; do
-        grep "$list2" $d/movies.list > "$d"/"$list2".list
-        if ! grep -q "$list2" "$d"/"$list2".list; then
-            rm "$d"/"$list2".list
-            echo "You have no $list2 movies! Fuck Yeah!"
-        elif [ "$list2" = "720p" ]; then
-            listupdate
-            echo "You have settled with $num movies at $list2 quality"
-        elif [ "$list2" = "1080p" ]; then
-            listupdate
-            echo "You have upgraded $num movies to $list2 quality"
-        elif [ "$list2" = "2160p" ]; then
-            listupdate
-            echo "$num movies have acended to $list2 quality"
-        else
-            listupdate
-            echo "You have $num $list2 movies left to upgrade"
-        fi
+    grep "$list2" $d/movies.list > "$d"/"$list2".list
+    if ! grep -q "$list2" "$d"/"$list2".list; then
+        rm "$d"/"$list2".list
+        echo "You have no $list2 movies"
+    else
+        listupdate
+        echo "You have $num $list2 movies"
+    fi
     done
 echo $spacer
 }
@@ -69,7 +52,6 @@ listloop() {
     echo $spacer
 }
 listupdate() {
-echo "Updated the $list2 list"
 num=$(wc -l < "$d"/"$list2".list)
 }
 dupeloop() {  # Prints all movies in list and their dupe. No Highlighting.
@@ -129,9 +111,6 @@ if [[ $# == 2 ]]; then
             arg2=movies
             dupeloop
             dupecheck
-        elif [ "$2" = "test" ]; then
-            dupeloop
-            dupecheck
         else
             dupeloop
             dupecheck
@@ -139,6 +118,3 @@ if [[ $# == 2 ]]; then
     fi
 exit 0
 fi
-echo "End of script"
-echo $spacer
-exit 0
