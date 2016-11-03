7d12e7ecfeda363f2c3012a1c0a0dbd42ebeac92
dirtycajunrice <nicholas.totsch@gmail.com>
2016-11-03 03:58:43 -0500

    updated to work explicitly with my setup on 14.04

--- bin/rndcreload.sh
+++ bin/rndcreload.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-sudo rndc reload
-sleep 4
-journalctl -u bind9 -n 200 | grep -e "dirtycajunrice.com" -e "10.0.10"
+
+# Check if ran by root
+if [[ $UID -ne 0 ]]; then
+    echo 'Script must be run as root'
+    exit 1
+fi
+
+# Reload bind9
+rndc reload
+
+# Print status to stdout for anything from dirtycajunrice.com or 10.0.10 and 3 lines back
+grep -e "dirtycajunrice.com" -e "10.0.10" /var/log/bind/bind.log
