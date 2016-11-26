7a32eeb5940b5f077b65a808bb4e9529053fc8d8
dirtycajunrice <nicholas.totsch@gmail.com>
2016-11-26 15:38:34 -0600

    wget instead of bash call

--- config_ini/nicksaliases.sh
+++ config_ini/nicksaliases.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Pullable alias for working on remote systems. bash https://dirtycajunrice.com/alias
+# Pullable alias for working on remote systems. wget -q https://dirtycajunrice.com/alias && source alias && rm alias
 alias whatami='if [[ -e /etc/redhat-release ]]; then cat /etc/redhat-release; else lsb_release -a; fi'
