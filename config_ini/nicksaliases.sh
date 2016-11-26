181cb6a66aa3fd0e20ee54d4213e851c1a2b0941
dirtycajunrice <nicholas.totsch@gmail.com>
2016-11-26 15:47:49 -0600

    functions, not aliases!

--- config_ini/nicksaliases.sh
+++ config_ini/nicksaliases.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-# Pullable alias for working on remote systems. wget -q https://dirtycajunrice.com/alias && source alias && rm alias
-alias whatami='if [[ -e /etc/redhat-release ]]; then cat /etc/redhat-release; else lsb_release -a; fi'
+# Pullable functions for working on remote systems. wget -q https://dirtycajunrice.com/alias && source alias && rm alias
+whatami ()
+{
+	if [[ -e /etc/redhat-release ]]; then 
+	    cat /etc/redhat-release
+	else
+	    lsb_release -a
+	fi
+}
