781d3e0048f7053fbffc75814ca30ac78caa7af8
Nicholas St. Germain <nick@cajun.pro>
2018-06-14 15:12:46 -0500

    Create certbot-route53-dns-auth.py

---
+++ certbot-route53-dns-auth.py
@@ -0,0 +1,34 @@
+import os
+import sleep
+import boto3
+
+domain = 'domain.tld.' # Trailing period required!
+
+client = boto3.client('route53')
+recordName = '_acme-challenge.' + domain
+zoneID = [ zone['Id'] for zone in client.list_hosted_zones_by_name()['HostedZones'] if zone['Name'] == domain ][0]
+record_precheck = client.test_dns_answer(HostedZoneId=zoneID, RecordName=recordName, RecordType='TXT')['RecordData']
+dns1_verification = [{'value': os.environ["CERTBOT_VALIDATION"]}]
+
+if record_precheck:
+    for record in record_precheck:
+        dns1_verification.append({'Value': record})
+
+response = client.change_resource_record_sets(
+    HostedZoneId=zoneID,
+    ChangeBatch={
+        'Changes': [
+            {
+                'Action': 'UPSERT',
+                'ResourceRecordSet': {
+                    'Name': recordName,
+                    'Type': 'TXT',
+                    'TTL': 300,
+                    'ResourceRecords': dns1_verification
+                }
+            }
+        ]
+    }
+)
+
+sleep(15)
