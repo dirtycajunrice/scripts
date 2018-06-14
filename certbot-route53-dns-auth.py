e2e2bd2e62e74468c7fd43b70ef1bd5c2860075f
Nicholas St. Germain <nick@cajun.pro>
2018-06-14 15:31:14 -0500

    Update certbot-route53-dns-auth.py

--- certbot-route53-dns-auth.py
+++ certbot-route53-dns-auth.py
@@ -2,11 +2,11 @@ import os
 from time import sleep
 import boto3
 
-domain = 'domain.tld.' # Trailing period required!
+domain = 'domain.tld' # Trailing period required!
 
 client = boto3.client('route53')
 recordName = '_acme-challenge.' + domain
-zoneID = [ zone['Id'] for zone in client.list_hosted_zones_by_name()['HostedZones'] if zone['Name'] == domain ][0]
+zoneID = [ zone['Id'] for zone in client.list_hosted_zones_by_name()['HostedZones'] if zone['Name'] in domain ][0]
 record_precheck = client.test_dns_answer(HostedZoneId=zoneID, RecordName=recordName, RecordType='TXT')['RecordData']
 dns1_verification = [{'value': os.environ["CERTBOT_VALIDATION"]}]
 
