30fd39aa09065c822d3670385447752d7a20ed13
Nicholas St. Germain <nick@cajun.pro>
2018-06-14 15:17:19 -0500

    Create certbot-route53-dns-cleanum.py

---
+++ certbot-route53-dns-cleanum.py
@@ -0,0 +1,26 @@
+import boto3
+
+domain = 'cajun.pro.' # Trailing period required!
+
+client = boto3.client('route53')
+recordName = '_acme-challenge.' + domain
+zoneID = [ zone['Id'] for zone in client.list_hosted_zones_by_name()['HostedZones'] if zone['Name'] == domain ][0]
+record_precheck = client.test_dns_answer(HostedZoneId=zoneID, RecordName=recordName, RecordType='TXT')['RecordData']
+
+if record_precheck:
+    response = client.change_resource_record_sets(
+        HostedZoneId=zoneID,
+        ChangeBatch={
+            'Changes': [
+                {
+                    'Action': 'DELETE',
+                    'ResourceRecordSet': {
+                        'Name': recordName,
+                        'Type': 'TXT',
+                        'TTL': 300,
+                        'ResourceRecords': dns1_verification
+                    }
+                }
+            ]
+        }
+    )
