4974381fae973c2df6c6ce055c9e88490ab6946f
Nicholas St. Germain <nicholas.totsch@gmail.com>
2019-05-20 08:56:21 -0500

    Update certbot-route53-dns-cleanup.py

--- certbot-route53-dns-cleanup.py
+++ certbot-route53-dns-cleanup.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-
 import boto3
 
-from time import sleep
 from os import environ as en
 
-# OS Environment mappings
 domain = en['CERTBOT_DOMAIN'].strip('*.') if en['CERTBOT_DOMAIN'].startswith('*.') else en['CERTBOT_DOMAIN']
 validation_token = en["CERTBOT_VALIDATION"]
 
@@ -14,25 +11,21 @@ recordName = '_acme-challenge.' + domain
 zoneID = [zone['Id'] for zone in client.list_hosted_zones_by_name()['HostedZones'] if domain in zone['Name']][0]
 record_precheck = client.test_dns_answer(HostedZoneId=zoneID, RecordName=recordName, RecordType='TXT')['RecordData']
 dns1_verification = [{'Value': '"{}"'.format(validation_token)}]
-
-if record_precheck:
-    for record in record_precheck:
-        dns1_verification.append({'Value': record})
+new_recordset = [{'Value': record} for record in record_precheck if record != dns1_verification]
 
 changes = {
     'Changes': [
         {
-            'Action': 'UPSERT',
+            'Action': 'DELETE',
             'ResourceRecordSet': {
                 'Name': recordName,
                 'Type': 'TXT',
                 'TTL': 300,
-                'ResourceRecords': dns1_verification
+                'ResourceRecords': new_recordset
             }
         }
     ]
 }
 
-response = client.change_resource_record_sets(HostedZoneId=zoneID, ChangeBatch=changes)
-
-sleep(15)
+if record_precheck:
+    response = client.change_resource_record_sets(HostedZoneId=zoneID, ChangeBatch=changes)
