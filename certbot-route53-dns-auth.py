7bf2fc1b2a8af0277f6ba11504385a225695648c
Nicholas St. Germain <nicholas.totsch@gmail.com>
2019-05-20 08:55:15 -0500

    Update certbot-route53-dns-auth.py

--- certbot-route53-dns-auth.py
+++ certbot-route53-dns-auth.py
@@ -1,34 +1,38 @@
-import os
-from time import sleep
+#!/usr/bin/env python
+
 import boto3
 
-domain = 'domain.tld'
+from time import sleep
+from os import environ as en
+
+# OS Environment mappings
+domain = en['CERTBOT_DOMAIN'].strip('*.') if en['CERTBOT_DOMAIN'].startswith('*.') else en['CERTBOT_DOMAIN']
+validation_token = en["CERTBOT_VALIDATION"]
 
 client = boto3.client('route53')
 recordName = '_acme-challenge.' + domain
-zoneID = [ zone['Id'] for zone in client.list_hosted_zones_by_name()['HostedZones'] if domain in zone['Name'] ][0]
+zoneID = [zone['Id'] for zone in client.list_hosted_zones_by_name()['HostedZones'] if domain in zone['Name']][0]
 record_precheck = client.test_dns_answer(HostedZoneId=zoneID, RecordName=recordName, RecordType='TXT')['RecordData']
-dns1_verification = [{'value': os.environ["CERTBOT_VALIDATION"]}]
+dns1_verification = [{'Value': '"{}"'.format(validation_token)}]
 
 if record_precheck:
     for record in record_precheck:
         dns1_verification.append({'Value': record})
 
-response = client.change_resource_record_sets(
-    HostedZoneId=zoneID,
-    ChangeBatch={
-        'Changes': [
-            {
-                'Action': 'UPSERT',
-                'ResourceRecordSet': {
-                    'Name': recordName,
-                    'Type': 'TXT',
-                    'TTL': 300,
-                    'ResourceRecords': dns1_verification
-                }
+changes = {
+    'Changes': [
+        {
+            'Action': 'UPSERT',
+            'ResourceRecordSet': {
+                'Name': recordName,
+                'Type': 'TXT',
+                'TTL': 300,
+                'ResourceRecords': dns1_verification
             }
-        ]
-    }
-)
+        }
+    ]
+}
+
+response = client.change_resource_record_sets(HostedZoneId=zoneID, ChangeBatch=changes)
 
 sleep(15)
