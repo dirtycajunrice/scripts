0568848e3e32e08f0d51c87d93c60438d9f88ce5
Nicholas St. Germain <nick@cajun.pro>
2020-08-22 18:15:07 -0500

    cleanup

--- README.md
+++ README.md
@@ -1,18 +1,20 @@
-# scripts
+# Utility Scripts
 
-All my customized scripts for my Ubuntu Server.
-This repository is a collection of the scripts 
-that need to be customized to have a well rounded 
-fully functioning ubuntu server from home. I have
-included a changelog that documents the entire 
-process from beginning to end. if you have any
-questions feel free to open an issue ill get back
-to you as quickly as i can. This is also my attempt
-to completely rely on systemd and have no upstart 
-scripts on my server whatsoever. if you follow my 
-how-to.info you should be able to go from start 
-to finish in less than 3 hours. 
+8/22/20 - This repo has had everything moved to the [archived](archived) folder, and
+will be brought out one by one as they are deemed still usable or are updated.
 
-I hold no licence on any of these products they are
-all all freely distributable.
 
+## [haste](bash/haste.sh)
+cli function to post data to [hastebin.com](https://hastebin.com) or similar.
+Add this to your .bash_aliases: 
+```
+Usage: haste [options] [file]
+
+Options:
+  -r, --raw      Get the raw link to the output.
+  -h, --help     Print this help text.
+
+Upload the contents of plaintext document to a hastebin.
+Invocation with no arguments takes input from stdin or pipe.
+haste server location can be changed with the $HASTE_SERVER environment variable
+```
---
+++ bash/haste.sh
@@ -0,0 +1,72 @@
+# Initially copied from https://github.com/diethnis/standalones/blob/master/hastebin.sh
+# Then cleaned up and modified for my use case
+haste () {
+  local __USAGE="Usage: haste [options] [file]
+
+Options:
+  -r, --raw      Get the raw link to the output.
+  -h, --help     Print this help text.
+
+Upload the contents of plaintext document to a hastebin.
+Invocation with no arguments takes input from stdin or pipe.
+haste server location can be changed with the \$HASTE_SERVER environment variable
+"
+	local CONTENTS OUTPUT RETURN RAW
+	local HS=${HASTE_SERVER:-hastebin.com}
+	while [[ $# -gt 0 ]]; do
+    local KEY="$1"
+    case $KEY in
+      -h|--help)
+        echo -n "$__USAGE"
+        return 1
+        ;;
+      -r|--raw)
+        RAW="/raw/"
+        shift
+        shift
+        ;;
+      *)
+        POSITIONAL+=("$1")
+        shift
+        ;;
+    esac
+  done
+
+  set -- "${POSITIONAL[@]}" # restore positional parameters
+	if [[ -p /dev/stdin ]]
+	then
+		CONTENTS=$(cat)
+	elif (( $# != 1 ))
+	then
+		echo -n "$__USAGE"
+		return 1
+	elif [[ -e $1 && ! -f $1 ]]
+	then
+		echo "Error: Not a regular file."
+		return 1
+	elif [[ ! -e $1 ]]
+	then
+		echo "Error: No such file."
+		return 1
+	elif (( $(stat -c %s "$1") > (512*1024**1) ))
+	then
+		echo "Error: File must be smaller than 512 KiB."
+		return 1
+	fi
+	if [[ -z "$CONTENTS" ]]
+	then
+		CONTENTS=$(cat "$1")
+	fi
+	OUTPUT=$(curl -sd "$CONTENTS" "https://${HS}/documents")
+	if (( $? == 0 )) && [[ $OUTPUT =~ \"key\" ]]
+	then
+		RETURN=$(jq -r .key <<< "$OUTPUT")
+		if [[ -n $RETURN ]]
+		then
+			echo "https://${HS}${RAW:-/}${RETURN}"
+			return 0
+		fi
+	fi
+	echo "Upload failed."
+	return 1
+}
